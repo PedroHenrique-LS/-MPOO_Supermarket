f6e5100876db52ab1619cce52d61e8d50f811e11
Pedro Henrique <ph.laurindo2020@gmail.com>
2024-07-14 19:21:53 -0300

    initial commit

---
+++ src/main/java/com/laurindo/MPOO_Supermarket/MpooSupermarketApplication.java
@@ -0,0 +1,13 @@
+package com.laurindo.MPOO_Supermarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MpooSupermarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MpooSupermarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/laurindo/MPOO_Supermarket/MpooSupermarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.laurindo.MPOO_Supermarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MpooSupermarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

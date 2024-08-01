1c16d9168cca39aa6c3314077a128f78a4ca713c
Pedro Henrique <ph.laurindo2020@gmail.com>
2024-08-01 13:32:47 -0300

    CRUD implemented in CaixaService

--- src/main/java/com/laurindo/MPOO_Supermarket/service/CaixaService.java
+++ src/main/java/com/laurindo/MPOO_Supermarket/service/CaixaService.java
@@ -1,8 +1,11 @@
 package com.laurindo.MPOO_Supermarket.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.laurindo.MPOO_Supermarket.entity.Caixa;
 import com.laurindo.MPOO_Supermarket.entity.Compra;
 import com.laurindo.MPOO_Supermarket.repository.CaixaRepository;
 
@@ -29,6 +32,31 @@ public class CaixaService  {
 		});
 		
 		return compraService.saveCompra(compra);
+	}
+	
+	public Caixa saveCaixa(Caixa caixa) {
+		return caixaRepository.save(caixa);
+	}
+	
+	public Caixa findCaixaById(String cpf) {
+		var caixa = caixaRepository.findById(cpf).orElseThrow(() -> new IllegalArgumentException("Produto de código: " + cpf + " não encontrado."));
+		return caixa;
+	}
+	
+	public List<Caixa> findAllCaixa() {
+		return caixaRepository.findAll();
+	}
+	
+	public Caixa updateCaixa(String cpf, Caixa caixaUpdated) {
+		var oldCaixa = findCaixaById(cpf);
+		oldCaixa.setNome(caixaUpdated.getNome());
+		return caixaRepository.save(oldCaixa);
+	}
+	
+	
+	public void deleteCaixaById(String cpf) {
+		var caixa = findCaixaById(cpf);
+		caixaRepository.delete(caixa);
 	}
 
 }
